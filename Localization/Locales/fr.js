095897d4eb9a898d2a6a1fdcb4f731cd6517e1e9
sandracasas <sandra.casas7@gmail.com>
2018-12-28 22:04:06 +0100

    Add links

--- Localization/Locales/fr.js
+++ Localization/Locales/fr.js
@@ -4,7 +4,7 @@ let locale_fr =
 	language_fr: "Français",
 	menu_home: "ACCUEIL",
 	paragraph_1: "Bienvenue sur mon site Internet!",
-	paragraph_2: "Je m'appelle Sandra et je suis étudiante en Localisation à l'Université de Genève. Ici, vous trouverez des résumés de différents contenus du cours sous Portfolio, ainsi que des exemples de certains TPs que nous avons effectués au cours du semestre. Merci pour votre visite !",
+	paragraph_2: "Je m'appelle Sandra et je suis étudiante en Localisation à l'<a href='https://www.unige.ch/' target='_blank'>Université de Genève.</a><br><br>Ici, vous trouverez des résumés de différents contenus du cours sous Portfolio, ainsi que des exemples de certains TPs que nous avons effectués au cours du semestre. Merci pour votre visite !<br><br><br>Vous avez une question ? <a href='mailto:sandra.casas@etu.unige.ch'>Contactez-moi !</a><br><br><br><br>Le cours de Localisation vous intéresse ? Jetez un oeil à la <a href='http://wadme.unige.ch:3149/pls/opprg/w_det_cours.debut?p_code_cours=BTM0912&p_plan_is=0&p_langue=1&p_frame=N&p_mode=PGC&p_annee=2018&p_suffixe=&p_grtri=9629' target='_blank'>description</a> du cours.<br><br><br><br>Apprenez-en plus sur ma Faculté <a href='https://www.unige.ch/fti/fr/' target='_blank'>ici.</a><br><img src='images/FTI_noir50.jpg' alt='Logo Faculté de Traduction et d'interprétation' width='300' height='150'/>",
 	paragraph_3: "Portfolio",
 	paragraph_4: "Localisation d'images",
 	paragraph_5: "Page bientôt disponible en français !",
